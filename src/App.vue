c6adad0bd4454689cd7abe6ea14315d36c3f4be3
MyUsername <alberto.baglivi@gmail.com>
2022-11-23 14:44:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script>
+import Circle from './components/Circle.vue';
+
+export default{
+
+components: {
+  Circle, 
+},
+  data(){
+    return{
+
+    }
+  }
+}
+
+
+
+
+</script>
+
+<template>
+
+<main>
+
+  <Circle />
+
+</main>
+
+</template>
+
+<style lang="scss">
+@use "./styles/general.scss" as *;
+
+</style>
---
+++ src/components/Circle.vue
@@ -0,0 +1,78 @@
+<script>
+
+export default{
+
+ name : 'Circle', 
+
+  data(){
+    return{
+        message : "ciao"
+    }
+  }
+}
+
+
+
+</script>
+
+<template>
+
+
+<div class="container">
+
+    <div class="box">
+        <div class="circle">95%</div>
+        <svg height="300" width="300">
+          <circle cx="150" cy="150" r="97" stroke="red" stroke-width="8px" fill="none" />
+       </svg>
+    </div>
+
+</div>
+
+
+</template>
+
+<style lang="scss" scoped>
+
+.container{
+    display: flex;
+    justify-content: center;
+    padding: 1rem;
+
+    img{
+        max-width: 100%;
+    }
+
+.box{
+    display: flex;
+   justify-content: center;
+   align-items: center;
+   position: relative;
+   height: 500px;
+   width: 500px;
+   background-color: greenyellow;
+
+.circle{
+    height: 200px; 
+    width: 200px; 
+    border-radius: 50%;
+    border: 8px solid lightpink; 
+    background-color: lightgreen;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+   
+}
+svg{
+        position: absolute;
+        transform: rotate(-90deg);
+        
+        circle{
+            stroke-dashoffset: 30;
+            stroke-dasharray: 609;
+        }
+    }
+}
+}
+
+</style>
